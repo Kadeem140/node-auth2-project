e8bd3b814bbfe5795ea7c82d48bbbcea3ac25189
Kadeem140 <Beckford.kadeem@yahoo.com>
2020-09-10 13:56:57 -0400

    What i hae done so far

---
+++ users/users-router.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const bcrypt = require("bcryptjs")
+const Users = require("./users-model")
+const jwt = require("jsonwebtoken")
+
+const router = express.router()
+
+router.get('/api/user', async (req, res, next) => {
+    try{
+        res
+    }
+})
